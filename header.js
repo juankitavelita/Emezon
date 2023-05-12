9059562c48f071e821e4f29c9a1d71b608456c39
juankitavelita <jarevalo@nigul.cide.es>
2023-05-12 19:09:01 +0200

    Nuevos logos + Javascript cambia logos

--- header.js
+++ header.js
@@ -1,6 +1,7 @@
-
 window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header');
-    header.classList.toggle('header2', window.scrollY > 0);
-  });
-  
+  const header = document.querySelector('.header');
+  const logoDiv = document.querySelector('.logo');
+  const logo = logoDiv.querySelector('img');
+  header.classList.toggle('header2', window.scrollY > 0);
+  logo.setAttribute('src', (window.scrollY > 0) ? 'Logo_emezon_flecha.png' : 'Logo_emezon.png');
+});
